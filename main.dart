af1cac9c70348fde872d6fd26d6d0641ca4ea895
William Viktorsson <wilvik05@edu.umea.se>
2024-10-07 16:09:16 +0200

    wip

--- main.dart
+++ main.dart
@@ -1,32 +1,37 @@
 import 'dart:io';
 
-void main(List<String> args) {
-  print("Välkommen");
-  print("Du har 5 val");
 
-  print("Val 1: ...");
-  print("Val 1: ...");
-  print("Val 1: ...");
-  print("Val 1: ...");
-  print("Val 1: ...");
 
-  var input = stdin.readLineSync();
-  print("du skrev in: " + (input ?? "ingenting"));
 
-  var number = int.tryParse(input!);
 
-  if(number!=null){
-    // handle val 1 ...
-    // do crud operation to repo
-    
-  }
 
-}
+
+
+
+
 
 class Person {
   String name;
-  Person({required this.name});
+  int ssn;
+  Person({required this.name, required this.ssn});
 }
+
+
+void main(List<String> args) {
+  var william = Person(ssn: 50,name: "will");
+  print(william.name);
+  print(william.ssn);
+}
+
+
+
+
+
+
+
+
+
+
 
 class Car {
   Person owner;
